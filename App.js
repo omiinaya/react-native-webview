4b6b0a166ea8c1bfb910a8683834653df1cafeb5
omiinaya <omiinaya@gmail.com>
2022-01-01 02:36:21 -0500

    attempting to fix reload. could be phone issue.

--- App.js
+++ App.js
@@ -2,10 +2,33 @@ import React, { Component } from 'react';
 import { WebView } from 'react-native-webview';
 
 class MyWeb extends Component {
+  constructor(props) {
+    super(props);
+    
+  }
+  webView = {
+    canGoBack: false,
+    ref: null,
+  };
+  onShouldStartLoadWithRequest = (navigator) => {
+    
+    this.webView.ref.stopLoading();
+    return false;
+
+};
   render() {
     return (
       <WebView
+        scalesPageToFit={true}
+        automaticallyAdjustContentInsets={true}
+        scrollEnabled={true}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
         source={{ uri: 'https://servantapp.herokuapp.com/' }}
+        ref={(webView) => {
+          this.webView.ref = webView;
+        }}
+        onShouldStartLoadWithRequest={this.onShouldStartLoadWithRequest}
       />
     );
   }
